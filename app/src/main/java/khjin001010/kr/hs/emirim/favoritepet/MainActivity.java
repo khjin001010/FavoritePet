fda385a7d8180be61db0be39494c174f04d10cb6
khjin001010 <khjin001010@naver.com>
2017-03-30 20:42:56 +0900

    #3 check

--- app/src/main/java/khjin001010/kr/hs/emirim/favoritepet/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/favoritepet/MainActivity.java
@@ -2,12 +2,59 @@ package khjin001010.kr.hs.emirim.favoritepet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+    CheckBox checkboxselect;
+    RadioGroup rg;
+    RadioButton radio_dog, radio_cat, radio_rabbit;
+    Button but_ok;
+    ImageView imgv_pet;
+    TextView txt_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkboxselect = (CheckBox)findViewById(R.id.checkbox_select);
+        txt_1 = (TextView)findViewById(R.id.text_1);
+        rg = (RadioGroup)findViewById(R.id.rg);
+        radio_dog = (RadioButton)findViewById(R.id.radio_dog);
+        radio_cat = (RadioButton)findViewById(R.id.radio_cat);
+        radio_rabbit = (RadioButton)findViewById(R.id.radio_rabbit);
+        but_ok = (Button)findViewById(R.id.but_ok);
+        imgv_pet = (ImageView)findViewById(R.id.imgv_pet);
+        checkboxselect.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(checkboxselect.isChecked()){
+            txt_1.setVisibility(View.VISIBLE);
+            rg.setVisibility(View.VISIBLE);
+            radio_dog.setVisibility(View.VISIBLE);
+            radio_cat.setVisibility(View.VISIBLE);
+            radio_rabbit.setVisibility(View.VISIBLE);
+            but_ok.setVisibility(View.VISIBLE);
+            imgv_pet.setVisibility(View.VISIBLE);
+        }
+        else{
+            txt_1.setVisibility(View.INVISIBLE);
+            rg.setVisibility(View.INVISIBLE);
+            radio_dog.setVisibility(View.INVISIBLE);
+            radio_cat.setVisibility(View.INVISIBLE);
+            radio_rabbit.setVisibility(View.INVISIBLE);
+            but_ok.setVisibility(View.INVISIBLE);
+            imgv_pet.setVisibility(View.INVISIBLE);
+        }
     }
 }
